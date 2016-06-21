46883d0f987497b6e9a5b8d21361a1a0542fa471
Thomas Powell <t.powell.meto@gmail.com>
2016-06-21 14:43:00 +0100

    Initial commit

---
+++ Assets/Scripts/MyKinect.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System;
+using System.IO;
+using freenect;
+
+public class MyKinect : MonoBehaviour
+{
+
+	private static int TEXTURE_SIZE = 1024;
+
+	private Kinect kinect;
+	private int kinectWidth;
+	private int kinectHeight;
+
+	private int terrainWidth;
+	private int terrainHeight;
+
+	public Texture2D depthTexture;
+	public GameObject terrain; //this is actually a 'Plane' object
+	public Material terrainMaterial;
+	public Shader terrainMaterialShader;
+
+	void Awake ()
+	{
+		Debug.Log ("Slowing overall framerate");
+		Application.targetFrameRate = 5;
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		if (freenect.Kinect.DeviceCount == 1) {
+			Debug.Log ("Initialising kinect");
+			kinect = new Kinect (0);
+			kinect.Open ();
+
+			kinect.DepthCamera.DataReceived += HandleKinectDepthCameraDataReceived;
+			kinect.DepthCamera.Mode = kinect.DepthCamera.Modes [5]; // use mm depth format
+
+			kinect.DepthCamera.Start ();
+			kinect.LED.Color = LEDColor.Red;
+
+			kinectWidth = kinect.DepthCamera.Mode.Width;
+			kinectHeight = kinect.DepthCamera.Mode.Height;
+
+			Debug.Log ("Started kinect with mode " + kinect.DepthCamera.Mode);
+			Debug.Log ("kinect frame data size " + kinect.DepthCamera.Mode.Size);
+			Debug.Log ("kinect data shape : Height [" + kinectHeight + "], Width[" + kinectWidth + "]");
+			Debug.Log ("kinect mode databitsperpixel " + kinect.DepthCamera.Mode.DataBitsPerPixel);
+			Debug.Log ("kinect mode paddingbitsperpixel " + kinect.DepthCamera.Mode.PaddingBitsPerPixel);
+
+			depthTexture = new Texture2D (kinectWidth, kinectHeight, TextureFormat.RGB565, false);
+
+			terrain = GameObject.Find ("Terrain");
+			terrainWidth = (kinectWidth /10) - 1;
+			terrainHeight = (kinectHeight /10) - 1;
+			terrain.transform.localScale = new Vector3 (terrainWidth, 1, terrainHeight);
+
+			terrainMaterial = terrain.GetComponent<Renderer>().material;
+			terrainMaterial.SetFloat ("_DepthTexWidth", kinectWidth);
+			terrainMaterial.SetFloat ("_DepthTexHeight", kinectHeight);
+			terrainMaterialShader = terrainMaterial.shader;
+
+		} else {
+			throw new Exception ("Could not initialise kinect as no devices were found.");
+		}
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		// Update status of accelerometer/motor etc.
+		kinect.UpdateStatus ();
+		// Process any pending events.
+		Kinect.ProcessEvents ();
+	}
+		
+	// Callback for kinect camera, called when depth data stream is received
+	private void HandleKinectDepthCameraDataReceived (object sender, BaseCamera.DataReceivedEventArgs e)
+	{
+//		byte[] kinectRawData = e.Data.Data;
+//		ushort[] kinectDepthValues = new ushort[320 * 240];
+//		for (int i = 0; i < kinectDepthValues.Length; i ++) {
+//			ushort s = BitConverter.ToUInt16 (e.Data.Data, i*2);
+//			kinectDepthValues [i] = s;
+//		}
+
+//		Debug.Log(BitConverter.IsLittleEndian);
+//		Debug.Log(BitConverter.ToUInt16 (e.Data.Data, 0));
+		depthTexture.LoadRawTextureData (e.Data.Data);
+		depthTexture.Apply();
+//		terrainMaterial.SetTexture ("_Heightmap", depthTexture);
+	}
+
+	//Used to tidy up when application is exited
+	void OnApplicationQuit ()
+	{
+		Debug.Log ("Application ending after " + Time.time + " seconds, closing kinect gracefully.");
+		kinect.LED.Color = LEDColor.BlinkGreen;
+		kinect.Close ();
+	}
+		
+}
---
+++ Assets/Shaders/MyUnlitShader.shader
@@ -0,0 +1,108 @@
+﻿Shader "Unlit/MyUnlitShader"
+{
+	Properties
+	{
+		_DepthTex ("Depth Map Texture (RGB565)", 2D) = "black" {}
+		_DepthTexWidth ("Depth Map Texture Width", Float) = 1.0
+		_DepthTexHeight ("Depth Map Texture Height", Float) = 1.0
+
+		_ColorTex ("Color Map Texture (RGB)", 2D) = "white" {}
+	}
+
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 200
+
+		Pass
+		{
+			CGPROGRAM
+
+			// Use the method name 'vert' for the vertex shader
+    		#pragma vertex vert            
+    		// Use the method named 'frag' for the fragment shader 
+    		#pragma fragment frag
+
+    		#pragma target 4.0
+
+    		// Include some magic from unity
+    		#include "UnityCG.cginc"
+
+    		sampler2D _DepthTex;
+    		float4 _DepthTex_ST;
+    		float _DepthTexWidth;
+    		float _DepthTexHeight;
+
+    		sampler2D _ColorTex;
+
+    		// Object gets instantiated and passed into the vertex shader method.
+    		struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+    		// Represents a class that gets instantiated in the vertex shader, populated 
+    		// and then passed as the input into the fragment shader
+		    struct v2f {
+          		float4 pos : SV_POSITION;
+          		float2 uv : TEXCOORD0;
+          		fixed4 color : COLOR;		// don't know if this is actually required?!
+      		};
+
+		
+
+//		    float decodeFloatRGB565(float3 enc) {
+//		    	    	
+//		    }
+
+//			float decodeFloatR16(float3 enc) {
+//				float r = enc.r;
+//			}
+
+      		// The vertex shader - handles the position of the vertex 
+		    v2f vert(appdata v) {
+		        v2f o;
+		        o.pos = mul(UNITY_MATRIX_MVP, v.vertex);
+		        o.uv = v.uv;
+
+		        float4 depthRGBA = tex2Dlod(_DepthTex, float4(v.uv, 0.0, 0.0));
+
+		        float r = depthRGBA.r;  // no data in this channel
+		        float g = depthRGBA.g;  // no data in this channel
+		        float b = depthRGBA.b;  // no data in this channel
+		        float a = depthRGBA.a;  // data!
+
+		        float val = a*16;
+		        o.pos.y = val;
+
+//				float scale = 1.0/255.0;
+//				float val = depth * scale;
+
+				o.color = float4(val,val,val,0.0);
+
+//				if(val > 0.05) {
+//				//red - above limit
+//				o.color = float4(1.0,0.0,0.0,0.0);
+//				} 
+//				else if (val == 0.0) {
+//				//blue - equal to 0
+//				o.color = float4(0.0,0.0,1.0,0.0);
+//				} else {
+//				//green - between 0 and limit
+//				o.color = float4(0.0,1.0,0.0,0.0);
+//				} 
+
+		        return o;
+		    }
+
+		    // The fragment shader - handles the color of vertex
+		    fixed4 frag (v2f i) : SV_Target { 
+		    	return i.color; 
+		    }
+
+
+		    ENDCG
+		}
+	}
+}
